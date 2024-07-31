ac027f248bfc1ea4f9311fb22b507717352b97ef
doanvantrong96 <101232627+doanvantrong96@users.noreply.github.com>
2024-08-01 00:20:46 +0700

    Update

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -26,14 +26,27 @@ return [
             'clientSecret' => 'ea9d2533cc1321dff69ad23e0653959a'
         ]
     ],
+    // 'fireBase' => [//Cấu hình firebase đăng nhập -> Dùng product cần đổi value các key
+    //     'login' => [
+    //         "apiKey" => "AIzaSyC7QCc1QHH6giib7hM3NK-LOzMpYsvCfrU",
+    //         "authDomain" => "testlogin-df35b.firebaseapp.com",
+    //         "databaseURL" => "testlogin-df35b.firebaseio.com",
+    //         "projectId" => "testlogin-df35b",
+    //         "storageBucket" => "testlogin-df35b.appspot.com",
+    //         "messagingSenderId" => "856383394380"
+    //     ]
+    // ]
+
     'fireBase' => [//Cấu hình firebase đăng nhập -> Dùng product cần đổi value các key
         'login' => [
-            "apiKey" => "AIzaSyC7QCc1QHH6giib7hM3NK-LOzMpYsvCfrU",
-            "authDomain" => "testlogin-df35b.firebaseapp.com",
-            "databaseURL" => "testlogin-df35b.firebaseio.com",
-            "projectId" => "testlogin-df35b",
-            "storageBucket" => "testlogin-df35b.appspot.com",
-            "messagingSenderId" => "856383394380"
+            "apiKey" => "AIzaSyCJvghh5Shh3x-xXToyRla6-xC1MxshtV0",
+            "authDomain" => "khocustomer-ac2b2.firebaseapp.com",
+            // "databaseURL" => "kho-527a3.firebaseio.com",
+            "projectId" => "khocustomer-ac2b2",
+            "storageBucket" => "khocustomer-ac2b2.appspot.com",
+            "messagingSenderId" => "757984201074",
+            "appId" => "1:757984201074:web:8aa6b2c06c8ccea500b5eb",
+            "measurementId" => "G-VW4CN87ZZS"
         ]
     ]
 ];
